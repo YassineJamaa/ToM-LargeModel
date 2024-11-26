1797ac132c94d18ef7995ec7769b695246c03c82
YassineJamaa <yassine.jamaa@epfl.ch>
2024-11-26 17:21:54 -0500

    Update dataset and src folders

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .task import LangLocDataset, ToMLocDataset
+from .task import LangLocDataset, ToMLocDataset, ExtendedTomLocGPT4
 from .huggingface_models import ImportLLM, LayersUnitsLLM
 from .assess import AssessBenchmark
 from src.localisation import LocImportantUnits
