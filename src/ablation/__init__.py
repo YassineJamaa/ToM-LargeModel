487991ac495ceb6d8286930f38eb7b6d03a20096
YassineJamaa <yassine.jamaa@epfl.ch>
2024-12-06 08:21:02 -0500

    add Gaussian Noise as Imputation

--- src/ablation/__init__.py
+++ src/ablation/__init__.py
@@ -1,2 +1,3 @@
 from src.ablation.zeroing import ZeroingAblation
 from src.ablation.mean_imputation import MeanImputation
+from src.ablation.gaussian_noise import GaussianNoise
--- src/ablation/gaussian_noise.py
+++ src/ablation/gaussian_noise.py
@@ -1,10 +1,13 @@
 from src.ablation.zeroing import ZeroingAblation
 import numpy as np
+import torch
 
-class MeanImputation(ZeroingAblation):
-    def __init__(self, mean_units: np.ndarray):
+class GaussianNoise(ZeroingAblation):
+    def __init__(self, mean_units: np.ndarray, std_err: float):
         super().__init__()
         self.mean_units = mean_units
+        self.std_err = std_err
+        self.noise = torch.normal(mean=0, std=self.std_err, size=self.mean_units.shape)
     
     def get_hook_ablate(self, idx, mask):
         """
@@ -20,6 +23,5 @@ class MeanImputation(ZeroingAblation):
         def hook_ablate(module, input, output):
             mask_layer = mask[idx]
             unit_indices = mask_layer.nonzero()[0]
-            output[0][:, :, unit_indices] = self.mean_units[unit_indices, idx]
+            output[0][:, :, unit_indices] = self.mean_units[unit_indices, idx] + self.noise[unit_indices, idx]
         return hook_ablate
-
